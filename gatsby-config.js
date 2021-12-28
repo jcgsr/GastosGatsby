d992dc4b1d00829bf75f5cc26c7965c138e0e29c
jcgsr <cabralguerra@gmail.com>
2021-12-28 15:59:31 -0300

    gatsby dotenv (gatsby-plugin-env-variables)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 // 18.12.2021 - Refazendo o site de gastos. agora em Gatsby.
 // Todos no Abais (Tãnia está aqui). Diana foi ontem com Frederico e Tânia e Susi foi hoje sozinha depois de insistir muito pra eu ir. Houve problema com a recarga da oi. Atrasou mais ainda.
 
-//require("dotenv").config({
-//path: `.env.${process.env.NODE_ENV}`,
-//});
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     title: `Gastos Gatsby`,
@@ -23,28 +23,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: "gatsby-plugin-firebase",
-    //   options: {
-    //     credentials: {
-    // apiKey: "AIzaSyCbdjmsUWfFooP-R9xfNB1mjNfgT2Q0mJQ",
-    // authDomain: "gastosvue.firebaseapp.com",
-    // databaseURL: "https://gastosvue.firebaseio.com",
-    // projectId: "gastosvue",
-    // storageBucket: "gastosvue.appspot.com",
-    // messagingSenderId: "1026932042493",
-    // appId: "1:1026932042493:web:907ec4b1ca28277baeac94",
-    // measurementId: "G-YNTNVK3Y79",
-    // apiKey: process.env.GATSBY_PUBLIC_FIREBASE_API_KEY,
-    // authDomain: `${process.env.GATSBY_PUBLIC_FIREBASE_PROJECT_ID}firebaseapp.com`,
-    // databaseURL: `https://${process.env.GATSBY_PUBLIC_FIREBASE_PROJECT_ID}.firebaseio.com`,
-    // projectId: process.env.GATSBY_PUBLIC_FIREBASE_PROJECT_ID,
-    // storageBucket: `${process.env.GATSBY_PUBLIC_FIREBASE_PROJECT_ID}.appspot.com`,
-    // messagingSenderId: "1026932042493",
-    // appId: "1:1026932042493:web:907ec4b1ca28277baeac94",
-    //   },
-    // },
-    // },
+    {
+     resolve: `gatsby-plugin-env-variables`,
+      options: {
+        allowList: ["GATSBY_PUBLIC_FIREBASE_API_KEY", "GATSBY_PUBLIC_FIREBASE_PROJECT_ID"]
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
