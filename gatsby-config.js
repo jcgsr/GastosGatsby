31ad4733a0d175df3ca9576ea12075dfc1c47a75
jcgsr <cabralguerra@gmail.com>
2023-01-26 09:33:12 -0300

    icon dollar sign

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,7 +56,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#272b30`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/cifrao.png`, // This path is relative to the root of the site.
       },
     },
   ],
